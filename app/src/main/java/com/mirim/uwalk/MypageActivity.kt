b65e90f98b137c4d285b515501ef22ecd77b3c4d
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-08-12 15:04:08 +0100

    Feat: edit gradle

--- app/src/main/java/com/mirim/uwalk/MypageActivity.kt
+++ app/src/main/java/com/mirim/uwalk/MypageActivity.kt
@@ -2,8 +2,11 @@ package com.mirim.uwalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mirim.uwalk.databinding.ActivityMypageBinding
 
 class MypageActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMypageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypage)
