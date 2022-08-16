c57d6d26d11cb59e6f078e4d8823d2bca2d4a5d1
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-08-16 12:45:10 +0100

    Feat: create record map + recyclerView

--- app/src/main/java/com/mirim/uwalk/RecordFragment.kt
+++ app/src/main/java/com/mirim/uwalk/RecordFragment.kt
@@ -6,18 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mirim.uwalk.databinding.FragmentRecordBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class RecordFragment: Fragment() {
     lateinit var binding: FragmentRecordBinding
 
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<CustomAdapter.ViewHolder>? = null
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRecordBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        layoutManager = LinearLayoutManager(context)
+
+        binding.recyclerView.layoutManager = layoutManager
+
+        adapter= CustomAdapter(context)
+
+        binding.recyclerView.adapter = adapter
         return view
     }
 }
